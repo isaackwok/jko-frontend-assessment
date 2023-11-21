2873e9e22d68f5dd395038376d8e391b658b71d5
Isaac Kwok <kch062522@gmail.com>
2023-11-22 00:56:32 +0800

    feat: finish basic carousel

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -7,15 +7,15 @@ export type CarouselProps = {
 export type CarouselComponent = React.FC<CarouselProps>
 
 export const Carousel: CarouselComponent = ({ imgUrls }) => {
-  const [currentImgIdx] = React.useState(0)
-  return <div className="relative w-full overflow-hidden aspect-square">
+  const [currentImgIdx, setCurrentImgIdx] = React.useState(0)
+  return <div className="relative w-full aspect-square">
     {
       imgUrls.length ? (
         <>
-          {imgUrls.map((img) => {
-            return <img className="h-full w-full object-cover object-center" key={img} src={img} />
-          })}
-          <span className="absolute right-3 bottom-3 py-1 px-2 bg-black/30 text-white">{currentImgIdx + 1}/{imgUrls.length}</span>
+          <img className="h-full w-full object-contain object-center bg-white" src={imgUrls[currentImgIdx]} />
+          <div className="absolute inset-y-0 left-0 w-1/2" onClick={() => setCurrentImgIdx(i => Math.max(i - 1, 0))} />
+          <div className="absolute inset-y-0 right-0 w-1/2" onClick={() => setCurrentImgIdx(i => Math.min(i + 1, imgUrls.length - 1))} />
+          <span className="absolute rounded-full right-3 bottom-3 py-1 px-2 bg-black/30 text-white">{currentImgIdx + 1}/{imgUrls.length}</span>
         </>
       ) : null
     }
