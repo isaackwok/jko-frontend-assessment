0f3e69638822311dad2323033131167b768b812a
Isaac Kwok <kch062522@gmail.com>
2023-11-24 22:16:46 +0800

    feat: persist cart

--- src/stores/shopping-cart.ts
+++ src/stores/shopping-cart.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import {persist} from "zustand/middleware";
 import { ItemVariation } from "../schemas/shop-item";
 import { z } from "zod";
 
@@ -12,9 +13,15 @@ export type ShoppingCartStore = {
   addItem: (item: ShoppingCartRecord) => void;
   removeItem: (itemIdx: number) => void;
 };
-export const useShoppingCartStore = create<ShoppingCartStore>((set) => ({
-  items: [],
-  addItem: (item: ShoppingCartRecord) =>
-    set((state) => ({ ...state, items: [...state.items, item] })),
-    removeItem: (itemIdx: number) => set((state) => ({ ...state, items: state.items.filter((_, idx) => idx !== itemIdx) })),
-}));
+export const useShoppingCartStore = create(
+  persist<ShoppingCartStore>(
+    (set) => ({
+      items: [],
+      addItem: (item: ShoppingCartRecord) =>
+        set((state) => ({ ...state, items: [...state.items, item] })),
+        removeItem: (itemIdx: number) => set((state) => ({ ...state, items: state.items.filter((_, idx) => idx !== itemIdx) })),
+    }), {
+      name: "shopping-cart",
+    }
+  )
+);
